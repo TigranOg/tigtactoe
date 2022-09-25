28055e20a1f97c634da4fa69876f9bdf55d4a504
Tiga <tigagalaxy@gmail.com>
2022-09-25 13:45:26 +0300

    impl minimax algorithm

--- lib/bloc/game_logic_event.dart
+++ lib/bloc/game_logic_event.dart
@@ -7,6 +7,7 @@ import 'package:tig_tag_toe/bloc/game_logic_state.dart';
 abstract class GameLogicEvent {
   Stream<GameLogicState> applyAsync({required GameLogicState currentState, required GameLogicBloc bloc});
 }
+
 class RestartMove extends GameLogicEvent {
   @override
   Stream<GameLogicState> applyAsync({required GameLogicState currentState, required GameLogicBloc bloc}) async* {
@@ -55,10 +56,7 @@ class AIMove extends GameLogicEvent {
       if (currentState is InGameState) {
         final Game3x3Model model = currentState.model;
 
-        Move move = MiniMaxAlgo.findBestMove(model.spaces, false, 0);
-
-        SpaceItem spaceItem = model.spaceByCoordinates(row: move.row, col: move.col);
-        spaceItem.itemState = ItemState.O;
+        MiniMaxAlgo.findBestMove(model.spaces, false);
 
         final int evaluation = MiniMaxAlgo.evaluate(model.spaces);
 
@@ -84,75 +82,77 @@ class Move {
 }
 
 class MiniMaxAlgo {
-  static Move findBestMove(List<List<SpaceItem>> spaces, bool isMaximizing, int depth) {
-    late Move move;
+  static findBestMove(List<List<SpaceItem>> spaces, bool isMaximizing) {
+    Move? move;
 
-    int bestValue = isMaximizing ? -1000 : 1000;
+    int currentScore = isMaximizing ? -1000 : 1000;
 
     for (int row = 0; row < 3; row++) {
       for (int col = 0; col < 3; col++) {
         if (spaces[row][col].itemState == ItemState.empty) {
 
-          spaces[row][col].itemState = ItemState.O;
+          spaces[row][col].itemState = isMaximizing ? ItemState.empty : ItemState.O;
 
-          int value = minimax(spaces, !isMaximizing, depth);
+          int score = minimax(spaces, !isMaximizing, 0);
 
           spaces[row][col].itemState = ItemState.empty;
 
           if (isMaximizing) {
-            if (bestValue < value) {
+            if (currentScore < score) {
               move = Move(row: row, col: col);
-              bestValue = value;
+              currentScore = score;
             }
           } else {
-            if (bestValue > value) {
+            if (currentScore > score) {
               move = Move(row: row, col: col);
-              bestValue = value;
+              currentScore = score;
             }
           }
+          print('currentScore: $currentScore');
+          print('----------------');
         }
       }
     }
-    return move;
+
+    if (move != null) {
+      spaces[move.row][move.col].itemState = ItemState.O;
+    }
   }
 
   static int minimax(List<List<SpaceItem>> spaces, bool isMaximizing, int depth) {
     final int evaluation = MiniMaxAlgo.evaluate(spaces);
 
-    if (evaluation != 0) {
+    if (evaluation != 0 || !isMovesLeft(spaces)) {
       //TODO add/sub depth
+      // return isMaximizing ? evaluation - depth : evaluation + depth;
       return evaluation;
     }
 
-    if (isMovesLeft(spaces)) {
-      return 0;
-    }
-
-    int bestValue = isMaximizing ? -1000 : 1000;
+    int currentScore = isMaximizing ? -1000 : 1000;
 
     for (int row = 0; row < 3; row++) {
       for (int col = 0; col < 3; col++) {
-        {
+        if (spaces[row][col].itemState == ItemState.empty) {
           spaces[row][col].itemState = isMaximizing ? ItemState.X : ItemState.O;
 
-          int value = minimax(spaces, !isMaximizing, depth);
+          int value = minimax(spaces, !isMaximizing, depth + 1);
 
           spaces[row][col].itemState = ItemState.empty;
 
           if (isMaximizing) {
-            if (bestValue < value) {
-              bestValue = value;
+            if (currentScore < value) {
+              currentScore = value - depth;
             }
           } else {
-            if (bestValue > value) {
-              bestValue = value;
+            if (currentScore > value) {
+              currentScore = value + depth;
             }
           }
         }
       }
     }
 
-    return bestValue;
+    return currentScore;
   }
 
   static bool isMovesLeft(List<List<SpaceItem>> spaces) {
@@ -172,9 +172,9 @@ class MiniMaxAlgo {
     for (int row = 0; row < 3; row++) {
       if (spaces[row][0].itemState == spaces[row][1].itemState && spaces[row][1].itemState == spaces[row][2].itemState) {
         if (spaces[row][0].itemState == ItemState.X) {
-          return 10;
+          return 100;
         } else if (spaces[row][0].itemState == ItemState.O) {
-          return -10;
+          return -100;
         }
       }
     }
@@ -183,9 +183,9 @@ class MiniMaxAlgo {
     for (int column = 0; column < 3; column++) {
       if (spaces[0][column].itemState == spaces[1][column].itemState && spaces[1][column].itemState == spaces[2][column].itemState) {
         if (spaces[0][column].itemState == ItemState.X) {
-          return 10;
+          return 100;
         } else if (spaces[0][column].itemState == ItemState.O) {
-          return -10;
+          return -100;
         }
       }
     }
@@ -193,17 +193,17 @@ class MiniMaxAlgo {
     //Check diagonals
     if (spaces[0][0].itemState == spaces[1][1].itemState && spaces[1][1].itemState == spaces[2][2].itemState) {
       if (spaces[0][0].itemState == ItemState.X) {
-        return 10;
+        return 100;
       } else if (spaces[0][0].itemState == ItemState.O) {
-        return -10;
+        return -100;
       }
     }
 
     if (spaces[2][0].itemState == spaces[1][1].itemState && spaces[1][1].itemState == spaces[0][2].itemState) {
       if (spaces[1][1].itemState == ItemState.X) {
-        return 10;
+        return 100;
       } else if (spaces[1][1].itemState == ItemState.O) {
-        return -10;
+        return -100;
       }
     }
 
--- lib/bloc/game_logic_model.dart
+++ lib/bloc/game_logic_model.dart
@@ -16,7 +16,23 @@ class Game3x3Model extends Equatable {
 
 class SpaceItem {
   SpaceItem({required this.col, required this.row})
-      : itemState = ItemState.empty;
+      : itemState = ItemState.empty {
+    // if (col == 0 && row == 0) {
+    //   itemState = ItemState.X;
+    // } else if (row == 0 && col == 1) {
+    //   itemState = ItemState.O;
+    // } else if (row == 1 && col == 0) {
+    //   itemState = ItemState.O;
+    // } else if (row == 1 && col == 1) {
+    //   itemState = ItemState.O;
+    // }else if (row == 1 && col == 2) {
+    //   itemState = ItemState.X;
+    // }else if (row == 2 && col == 0) {
+    //   itemState = ItemState.X;
+    // }else if (row == 2 && col == 1) {
+    //   //itemState = ItemState.X;
+    // }
+  }
 
   final int row, col;
   ItemState itemState;
